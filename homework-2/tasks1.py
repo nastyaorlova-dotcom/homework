7f716c7f360b64f72dc407ab7f32a13a68dec716
nastyaorlova-dotcom <nastya.07039@gmail.com>
2026-02-23 19:55:36 +0300

    Update tasks1.py

--- homework-2/tasks1.py
+++ homework-2/tasks1.py
@@ -1,1 +1,14 @@
 
+text = input("Введите числа через пробел: ")
+items = text.split()
+s = int(input("Введите степень: "))
+r = []
+for x in items:
+    if x.isdigit() or (x[0] == '-' and x[1:].isdigit()):
+        n = int(x)
+        r.append(n ** s)
+    else:
+        r.append(x * s)
+print("Вывод:", end=" ")
+for y in r:
+    print(y, end=" ")

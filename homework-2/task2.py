b6fd1cb22742212fd0b3c9ae004fc01da35876f4
nastyaorlova-dotcom <nastya.07039@gmail.com>
2026-02-23 19:59:38 +0300

    Update task2.py

--- homework-2/task2.py
+++ homework-2/task2.py
@@ -1,1 +1,7 @@
-
+dct = {1: 11, 2: 22, 3: 33, 4: 4, 5: 33, 6: 1}
+keys_set = set(dct.keys())
+values_set = set(dct.values())
+union_set = keys_set | values_set
+print(f"Множество ключей: {keys_set}")
+print(f"Множество значений: {values_set}")
+print(f"Объединение множеств: {union_set}")
